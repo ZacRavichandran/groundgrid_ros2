0cf8d5cd1f6c9f22da0ed9ee746b7ff02de77e94
frankgon1627 <fg1333762@gmail.com>
2025-03-05 23:17:52 -0500

    took out tf2 from cpp

--- src/GroundGrid.cpp
+++ src/GroundGrid.cpp
@@ -3,11 +3,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include <grid_map_cv/GridMapCvConverter.hpp>
 #include <grid_map_core/GridMapMath.hpp>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+// #include <tf2/LinearMath/Matrix3x3.h>
+// #include <tf2/LinearMath/Quaternion.h>
+// #include <tf2_ros/buffer.h>
+// #include <tf2_ros/transform_listener.h>
+// #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 using namespace groundgrid;
 
