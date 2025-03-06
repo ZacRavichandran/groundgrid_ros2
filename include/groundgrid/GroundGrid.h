21b5ae2a3231120b817ce97b8b0f533ce5678b59
frankgon1627 <fg1333762@gmail.com>
2025-03-06 00:09:45 -0500

    refactored class structure

--- include/groundgrid/GroundGrid.h
+++ include/groundgrid/GroundGrid.h
@@ -1,42 +1,47 @@
 #pragma once
 
 // Grid map
-// #include <grid_map_ros/grid_map_ros.hpp>
-// #include <grid_map_msgs/msg/grid_map.hpp>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_msgs/msg/grid_map.hpp>
 
 // ROS 2 messages
-// #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
-// #include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 
 // TF2
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-// #include <geometry_msgs/msg/point_stamped.hpp>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#ifndef GROUNDGRID_GROUNDGRID_H
+#define GROUNDGRID_GROUNDGRID_H
 
 namespace groundgrid {
 
 class GroundGrid {
 public:
-    GroundGrid();
-    // GroundGrid(tf2_ros::Buffer& tf_buffer, tf2_ros::TransformListener& tf_listener);
+    // GroundGrid();
+    GroundGrid(tf2_ros::Buffer& tf_buffer, tf2_ros::TransformListener& tf_listener);
 
-    // virtual ~GroundGrid();
-    // void initGroundGrid(const nav_msgs::msg::Odometry::SharedPtr inOdom);
+    virtual ~GroundGrid();
+    void initGroundGrid(const nav_msgs::msg::Odometry::SharedPtr inOdom);
     
-    // std::shared_ptr<grid_map::GridMap> update(const nav_msgs::msg::Odometry::SharedPtr inOdom);
+    std::shared_ptr<grid_map::GridMap> update(const nav_msgs::msg::Odometry::SharedPtr inOdom);
 
-    // const float mResolution = .33f;
-    // const float mDimension = 120.0f;
+    const float mResolution = .33f;
+    const float mDimension = 120.0f;
 
 private:
-    // tf2_ros::Buffer& tf_buffer_;
-    // tf2_ros::TransformListener& tf_listener_;
+    tf2_ros::Buffer& tf_buffer_;
+    tf2_ros::TransformListener& tf_listener_;
 
-    // double mDetectionRadius = 60.0;
-    // std::shared_ptr<grid_map::GridMap> mMap_ptr;
-    // geometry_msgs::msg::TransformStamped mTfPosition, mTfLux, mTfUtm, mTfMap;
-    // geometry_msgs::msg::PoseWithCovarianceStamped mLastPose;
+    double mDetectionRadius = 60.0;
+    std::shared_ptr<grid_map::GridMap> mMap_ptr;
+    geometry_msgs::msg::TransformStamped mTfPosition, mTfLux, mTfUtm, mTfMap;
+    geometry_msgs::msg::PoseWithCovarianceStamped mLastPose;
 };
 
 }
+
+#endif // GROUNDGRID_GROUNDGRID_H
--- src/GroundGrid.cpp
+++ src/GroundGrid.cpp
@@ -1,123 +1,110 @@
 #include <groundgrid/GroundGrid.h>
-// #include <chrono>
-// #include <rclcpp/rclcpp.hpp>
-// #include <grid_map_cv/GridMapCvConverter.hpp>
-// #include <grid_map_core/GridMapMath.hpp>
-// #include <tf2/LinearMath/Matrix3x3.h>
-// #include <tf2/LinearMath/Quaternion.h>
-// #include <tf2_ros/buffer.h>
-// #include <tf2_ros/transform_listener.h>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <chrono>
+#include <rclcpp/rclcpp.hpp>
+#include <grid_map_cv/GridMapCvConverter.hpp>
+#include <grid_map_core/GridMapMath.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 using namespace groundgrid;
 
-    GroundGrid::GroundGrid() {}
-    // GroundGrid(tf2_ros::Buffer& tf_buffer, tf2_ros::TransformListener& tf_listener)
-        // : tf_buffer_(tf_buffer), tf_listener_(tf_listener) {}
+// GroundGrid::GroundGrid() {}
+    
+GroundGrid::GroundGrid(tf2_ros::Buffer& tf_buffer, tf2_ros::TransformListener& tf_listener)
+        : tf_buffer_(tf_buffer), tf_listener_(tf_listener) {}
 
-    // ~GroundGrid() {}
+GroundGrid::~GroundGrid() {}
 
-    // void initGroundGrid(const std::shared_ptr<nav_msgs::msg::Odometry> &inOdom) {
-    //     std::chrono::_V2::steady_clock::time_point start = std::chrono::steady_clock::now();
+void GroundGrid::initGroundGrid(const nav_msgs::msg::Odometry::SharedPtr inOdom) {
+    std::chrono::_V2::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-    //     geometry_msgs::msg::PoseWithCovarianceStamped odomPose;
+    geometry_msgs::msg::PoseWithCovarianceStamped odomPose;
 
-    //     mMap_ptr = std::make_shared<grid_map::GridMap>(std::vector<std::string>{"points", "ground", "groundpatch", "minGroundHeight", "maxGroundHeight"});
-    //     grid_map::GridMap &map = *mMap_ptr;
-    //     map.setFrameId("map");
-    //     map.setGeometry(grid_map::Length(mDimension, mDimension), mResolution,
-    //                     grid_map::Position(inOdom->pose.pose.position.x, inOdom->pose.pose.position.y));
+    mMap_ptr = std::make_shared<grid_map::GridMap>(std::vector<std::string>{"points", "ground", "groundpatch", "minGroundHeight", "maxGroundHeight"});
+    grid_map::GridMap &map = *mMap_ptr;
+    map.setFrameId("map");
+    map.setGeometry(grid_map::Length(mDimension, mDimension), mResolution,
+                    grid_map::Position(inOdom->pose.pose.position.x, inOdom->pose.pose.position.y));
 
-    //     RCLCPP_INFO(rclcpp::get_logger("groundgrid"), "Created map with size %f x %f m (%i x %i cells).",
-    //                 map.getLength().x(), map.getLength().y(),
-    //                 map.getSize()(0), map.getSize()(1));
+    RCLCPP_INFO(rclcpp::get_logger("groundgrid"), "Created map with size %f x %f m (%i x %i cells).",
+                map.getLength().x(), map.getLength().y(),
+                map.getSize()(0), map.getSize()(1));
 
-    //     odomPose.pose = inOdom->pose;
-    //     odomPose.header = inOdom->header;
-    //     std::vector<grid_map::BufferRegion> damage;
-    //     map.move(grid_map::Position(odomPose.pose.pose.position.x, odomPose.pose.pose.position.y), damage);
+    odomPose.pose = inOdom->pose;
+    odomPose.header = inOdom->header;
+    std::vector<grid_map::BufferRegion> damage;
+    map.move(grid_map::Position(odomPose.pose.pose.position.x, odomPose.pose.pose.position.y), damage);
 
-    //     map["points"].setZero();
-    //     map["ground"].setConstant(inOdom->pose.pose.position.z);
-    //     map["groundpatch"].setConstant(0.0000001);
-    //     map["minGroundHeight"].setConstant(100.0);
-    //     map["maxGroundHeight"].setConstant(-100.0);
+    map["points"].setZero();
+    map["ground"].setConstant(inOdom->pose.pose.position.z);
+    map["groundpatch"].setConstant(0.0000001);
+    map["minGroundHeight"].setConstant(100.0);
+    map["maxGroundHeight"].setConstant(-100.0);
 
-    //     std::chrono::_V2::steady_clock::time_point end = std::chrono::steady_clock::now();
-    //     RCLCPP_DEBUG(rclcpp::get_logger("groundgrid"), "Transforms lookup took %ld ms",
-    //                 std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+    std::chrono::_V2::steady_clock::time_point end = std::chrono::steady_clock::now();
+    RCLCPP_DEBUG(rclcpp::get_logger("groundgrid"), "Transforms lookup took %ld ms",
+                std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
 
-    //     mLastPose = odomPose;
-    // }
+    mLastPose = odomPose;
+}
 
-    // std::shared_ptr<grid_map::GridMap> update(const std::shared_ptr<nav_msgs::msg::Odometry> &inOdom) {
-    //     if (!mMap_ptr) {
-    //         initGroundGrid(inOdom);
-    //         return mMap_ptr;
-    //     }
+std::shared_ptr<grid_map::GridMap> GroundGrid::update(const nav_msgs::msg::Odometry::SharedPtr inOdom) {
+    if (!mMap_ptr) {
+        initGroundGrid(inOdom);
+        return mMap_ptr;
+    }
 
-    //     std::chrono::_V2::steady_clock::time_point start = std::chrono::steady_clock::now();
-    //     grid_map::GridMap &map = *mMap_ptr;
+    std::chrono::_V2::steady_clock::time_point start = std::chrono::steady_clock::now();
+    grid_map::GridMap &map = *mMap_ptr;
 
-    //     geometry_msgs::msg::PoseWithCovarianceStamped poseDiff;
-    //     poseDiff.pose.pose.position.x = inOdom->pose.pose.position.x - mLastPose.pose.pose.position.x;
-    //     poseDiff.pose.pose.position.y = inOdom->pose.pose.position.y - mLastPose.pose.pose.position.y;
+    geometry_msgs::msg::PoseWithCovarianceStamped poseDiff;
+    poseDiff.pose.pose.position.x = inOdom->pose.pose.position.x - mLastPose.pose.pose.position.x;
+    poseDiff.pose.pose.position.y = inOdom->pose.pose.position.y - mLastPose.pose.pose.position.y;
 
-    //     std::vector<grid_map::BufferRegion> damage;
-    //     map.move(grid_map::Position(inOdom->pose.pose.position.x, inOdom->pose.pose.position.y), damage);
+    std::vector<grid_map::BufferRegion> damage;
+    map.move(grid_map::Position(inOdom->pose.pose.position.x, inOdom->pose.pose.position.y), damage);
 
-    //     // Static so if the new transform is not yet available, we can use the last one
-    //     static geometry_msgs::msg::TransformStamped base_to_map;
+    // Static so if the new transform is not yet available, we can use the last one
+    static geometry_msgs::msg::TransformStamped base_to_map;
 
-    //     try {
-    //         base_to_map = tf_buffer_.lookupTransform("base_link", "map", tf2::TimePointZero);
-    //     } catch (const tf2::LookupException &e) {
-    //         RCLCPP_WARN(rclcpp::get_logger("groundgrid"), "No transform available: %s", e.what());
-    //     } catch (const tf2::ExtrapolationException &e) {
-    //         RCLCPP_DEBUG(rclcpp::get_logger("groundgrid"), "Extrapolation required: %s", e.what());
-    //     }
+    try {
+        base_to_map = tf_buffer_.lookupTransform("base_link", "map", tf2::TimePointZero);
+    } catch (const tf2::LookupException &e) {
+        RCLCPP_WARN(rclcpp::get_logger("groundgrid"), "No transform available: %s", e.what());
+    } catch (const tf2::ExtrapolationException &e) {
+        RCLCPP_DEBUG(rclcpp::get_logger("groundgrid"), "Extrapolation required: %s", e.what());
+    }
 
-    //     geometry_msgs::msg::PointStamped ps;
-    //     ps.header = inOdom->header;
-    //     ps.header.frame_id = "map";
-    //     grid_map::Position pos;
+    geometry_msgs::msg::PointStamped ps;
+    ps.header = inOdom->header;
+    ps.header.frame_id = "map";
+    grid_map::Position pos;
 
-    //     for (auto region : damage) {
-    //         for (auto it = grid_map::SubmapIterator(map, region); !it.isPastEnd(); ++it) {
-    //             auto idx = *it;
+    for (auto region : damage) {
+        for (auto it = grid_map::SubmapIterator(map, region); !it.isPastEnd(); ++it) {
+            auto idx = *it;
 
-    //             map.getPosition(idx, pos);
-    //             ps.point.x = pos(0);
-    //             ps.point.y = pos(1);
-    //             ps.point.z = 0;
-    //             tf2::doTransform(ps, ps, base_to_map);
-    //             map.at("ground", idx) = -ps.point.z;
-    //             map.at("groundpatch", idx) = 0.0;
-    //         }
-    //     }
+            map.getPosition(idx, pos);
+            ps.point.x = pos(0);
+            ps.point.y = pos(1);
+            ps.point.z = 0;
+            tf2::doTransform(ps, ps, base_to_map);
+            map.at("ground", idx) = -ps.point.z;
+            map.at("groundpatch", idx) = 0.0;
+        }
+    }
 
-    //     // If no movement, no update needed
-    //     if (damage.empty())
-    //         return mMap_ptr;
+    // If no movement, no update needed
+    if (damage.empty())
+        return mMap_ptr;
 
-    //     mLastPose.pose = inOdom->pose;
-    //     mLastPose.header = inOdom->header;
+    mLastPose.pose = inOdom->pose;
+    mLastPose.header = inOdom->header;
 
-    //     map.convertToDefaultStartIndex();
-    //     std::chrono::_V2::steady_clock::time_point end = std::chrono::steady_clock::now();
-    //     RCLCPP_DEBUG(rclcpp::get_logger("groundgrid"), "Total processing time: %ld ms",
-    //                 std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+    map.convertToDefaultStartIndex();
+    std::chrono::_V2::steady_clock::time_point end = std::chrono::steady_clock::now();
+    RCLCPP_DEBUG(rclcpp::get_logger("groundgrid"), "Total processing time: %ld ms",
+                std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
 
-    //     return mMap_ptr;
-    // }
-
-// private:
-    // tf2_ros::Buffer& tf_buffer_;
-    // tf2_ros::TransformListener& tf_listener_;
-
-    // std::shared_ptr<grid_map::GridMap> mMap_ptr;
-    // geometry_msgs::msg::PoseWithCovarianceStamped mLastPose;
-
-    // double mDimension = 10.0;
-    // double mResolution = 0.1;
-// };
+    return mMap_ptr;
+}
