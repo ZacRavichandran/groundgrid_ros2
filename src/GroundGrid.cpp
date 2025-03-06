62d5c6f967bdd0cba6f7d8c41eddf0fb22cf1e32
frankgon1627 <fg1333762@gmail.com>
2025-03-05 23:33:40 -0500

    removing namespace

--- src/GroundGrid.cpp
+++ src/GroundGrid.cpp
@@ -9,7 +9,7 @@
 // #include <tf2_ros/transform_listener.h>
 // #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
-using namespace groundgrid;
+// using namespace groundgrid;
 
 class GroundGrid {
 public:
