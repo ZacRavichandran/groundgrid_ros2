91dcdaf9e0224dae1e32e28a642436b1617aea0e
frankgon1627 <fg1333762@gmail.com>
2025-03-05 23:19:07 -0500

    took out tf2 from headder

--- include/groundgrid/GroundGrid.h
+++ include/groundgrid/GroundGrid.h
@@ -9,10 +9,10 @@
 #include <nav_msgs/msg/odometry.hpp>
 
 // TF2
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
+// #include <tf2_ros/buffer.h>
+// #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/msg/point_stamped.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+// #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 namespace groundgrid {
 
