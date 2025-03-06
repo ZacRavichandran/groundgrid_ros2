eedb6644063d8938f934a9b9f9fe84eec527daae
frankgon1627 <fg1333762@gmail.com>
2025-03-05 22:38:24 -0500

    testing name changes in launch

--- launch/ground_grid.launch.py
+++ launch/ground_grid.launch.py
@@ -12,18 +12,18 @@ def generate_launch_description():
             description='Point cloud topic to subscribe to'
         ),
 
-        # Component container
+        # Component container with a unique name
         launch_ros.actions.ComposableNodeContainer(
-            name='groundgrid_container',
+            name='groundgrid_container_node', 
+            namespace='groundgrid_ns',  
             package='rclcpp_components',
             executable='component_container',
-            namespace='',
             output='screen',
             composable_node_descriptions=[
                 launch_ros.descriptions.ComposableNode(
                     package='groundgrid',
                     plugin='groundgrid::GroundGridNode',
-                    name='groundgrid_node',
+                    name='groundgrid_node',  
                     remappings=[
                         ('/sensors/velodyne_points', LaunchConfiguration('point_cloud_topic'))
                     ]
