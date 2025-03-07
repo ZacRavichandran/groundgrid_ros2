590db22e4e031ac0b9154539a2706a63d2f645a7
frankgon1627 <fg1333762@gmail.com>
2025-03-06 22:26:13 -0500

    bigger buffer

--- src/GroundGridNode.cpp
+++ src/GroundGridNode.cpp
@@ -78,7 +78,7 @@ protected:
             return;
 
         try {
-            mapToBaseTransform = mTfBuffer_.lookupTransform("map", "base_link", tf2::TimePointZero, std::chrono::milliseconds(50));
+            mapToBaseTransform = mTfBuffer_.lookupTransform("map", "base_link", tf2::TimePointZero, std::chrono::milliseconds(500));
             cloudOriginTransform = mTfBuffer_.lookupTransform("map", "os_sensor", tf2::TimePointZero);
         }
         catch (const tf2::TransformException &ex) {
