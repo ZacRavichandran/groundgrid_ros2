606d313d77a11bf806e6a662559a27904daf453e
frankgon1627 <fg1333762@gmail.com>
2025-04-08 15:43:19 -0400

    putting groundgrid into ouster container

--- launch/ground_grid.launch.py
+++ launch/ground_grid.launch.py
@@ -1,42 +1,40 @@
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch_ros.descriptions import ComposableNode
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import LoadComposableNodes
 
 def generate_launch_description():
-    container_name = 'groundgrid_component_container'
-    namespace = 'groundgrid_ns'
+    container_name = LaunchConfiguration('container_name', default='/ouster/os_container')
+    pointcloud_topic = LaunchConfiguration('pointcloud_topic', default='/ouster/points')
 
-    return launch.LaunchDescription([
-        # Declare the point_cloud_topic argument
-        DeclareLaunchArgument(
-            'point_cloud_topic',
-            default_value='/ouster/points',
-            description='Point cloud topic to subscribe to'
-        ),
+    declare_container_name_arg = DeclareLaunchArgument(
+        'container_name',
+        default_value=container_name,
+        description='Name of the container to load nodes into'
+    )
+    declare_pointcloud_topic_arg = DeclareLaunchArgument(
+        'pointcloud_topic',
+        default_value=pointcloud_topic,
+        description='Pointcloud topic name'
+    )
 
-        # Component container with a unique name
-        launch_ros.actions.ComposableNodeContainer(
-            name=container_name, 
-            namespace=namespace, 
-            package='rclcpp_components',
-            executable='component_container',
-            output='screen'
-        ),
+    load_composable_nodes = LoadComposableNodes(
+        target_container=container_name,
+        composable_node_descriptions=[
+            ComposableNode(
+                package='groundgrid',
+                plugin='groundgrid::GroundGridNode',
+                name='groundgrid_node',
+                remappings=[
+                    ('/sensors/velodyne_points', pointcloud_topic)
+                ]
+            )
+        ]
+    )
 
-        # Load the component explicitly
-        LoadComposableNodes(
-            target_container=f'/{namespace}/{container_name}',
-            composable_node_descriptions=[
-                launch_ros.descriptions.ComposableNode(
-                    package='groundgrid',
-                    plugin='groundgrid::GroundGridNode',
-                    name='groundgrid_node',
-                    remappings=[
-                        ('/sensors/velodyne_points', LaunchConfiguration('point_cloud_topic'))
-                    ]
-                )
-            ]
-        )
+    return LaunchDescription([
+        declare_pointcloud_topic_arg,
+        declare_container_name_arg,
+        load_composable_nodes,
     ])
