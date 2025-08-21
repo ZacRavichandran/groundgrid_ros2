163bec31135bb56c465d756676fdbbf752d73e63
zac <>
2025-08-21 18:11:55 -0400

    update launch

--- launch/ground_grid.launch.py
+++ launch/ground_grid.launch.py
@@ -5,8 +5,15 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import LoadComposableNodes
 
 def generate_launch_description():
+    namespace = LaunchConfiguration('namespace')
     container_name = LaunchConfiguration('container_name', default='/ouster/os_container')
-    pointcloud_topic = LaunchConfiguration('pointcloud_topic', default='/ouster/points')
+    pointcloud_topic = LaunchConfiguration('pointcloud_topic', default='ouster/points')
+
+    declare_namespace_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='j100_0000',
+        description='Namespace for all nodes and topics'
+    )
 
     declare_container_name_arg = DeclareLaunchArgument(
         'container_name',
@@ -20,12 +27,13 @@ def generate_launch_description():
     )
 
     load_composable_nodes = LoadComposableNodes(
-        target_container=container_name,
+        target_container=[namespace, container_name],
         composable_node_descriptions=[
             ComposableNode(
                 package='groundgrid',
                 plugin='groundgrid::GroundGridNode',
                 name='groundgrid_node',
+                namespace=namespace,
                 remappings=[
                     ('/sensors/velodyne_points', pointcloud_topic)
                 ]
@@ -34,6 +42,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        declare_namespace_arg,
         declare_pointcloud_topic_arg,
         declare_container_name_arg,
         load_composable_nodes,
